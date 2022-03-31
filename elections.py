8062dedd213f51cf2d7b692c88299b27b5c42685
Michał Czuba <michal.czuba.1995@gmail.com>
2022-03-31 19:25:13 +0200

    crafting elections example

--- elections.py
+++ elections.py
@@ -2,6 +2,7 @@
 import configparser
 from typing import Any
 
+import networkx as nx
 import numpy as np
 np.random.seed(0)  # fix all seeds of the numpy which is used in the library
 
@@ -11,14 +12,6 @@ from network_diffusion import (
     PropagationModel,
 )
 
-
-def set_node_state(
-    experiment: MultiSpreading, layer_name: str, node_name: Any, state: str
-) -> None:
-    """Allows to set up the initial state of certain node."""
-    experiment._network.layers[layer_name].nodes[node_name]["status"] = state
-
-
 # read global config
 config = configparser.ConfigParser()
 config.read("config.ini")
@@ -26,35 +19,39 @@ output_dir = config.get("PATHS", "output_dir")
 
 # initialise graph
 network = MultilayerNetwork()
-network.load_mlx("aux/florentine.mpx")
-network.describe()
+network.load_layer_nx(nx.erdos_renyi_graph(150, 0.1), ["candidate_a", "candidate_b"])
+
+# candidate_a uses "positive" propaganda, i.e. he gains popularity by promotinh himself
+# candidate_b uses "negative" propaganda, i.e. he gains popularity by blackmailing candidate_a
 
 # initialise propagation model
 model = PropagationModel()
-phenomenas = [["S", "I"], ["UV", "V"]]
-for l, p in zip(network.get_nodes_states(), phenomenas):
-    model.add(l, p)
+model.add("candidate_a", ["negative", "neutral", "positive"])
+model.add("candidate_b", ["negative", "neutral", "positive"])
 
-# set possible transitions with weights
+# by default all transition are pseudo-probability equals to 0
 model.compile(background_weight=0.01)
 
-model.set_transition_fast("marriage.S", "marriage.I", ["business.UV"], 0.9)
-model.set_transition_fast("marriage.S", "marriage.I", ["business.V"], 0.3)
+# attitude to candidate_a changes having attitude to candidate_b constantly neutral
+model.set_transition_fast("candidate_a.neutral", "candidate_a.positive", ["candidate_b.neutral"], 0.1)
+model.set_transition_fast("candidate_b.neutral", "candidate_b.negative", ["candidate_a.neutral"], 0.1)
 
-model.set_transition_fast("business.UV", "business.V", ["marriage.S"], 0.05)
-model.set_transition_fast("business.UV", "business.V", ["marriage.I"], 0.03)
-model.set_transition_fast("business.V", "business.UV", ["marriage.S"], 0)
-model.set_transition_fast("business.V", "business.UV", ["marriage.I"], 0)
+# attitude to candidate_b changes having attitude to candidate_a constantly neutral
+model.set_transition_fast("candidate_b.neutral", "candidate_b.positive", ["candidate_a.neutral"], 0.1)
+model.set_transition_fast("candidate_a.neutral", "candidate_a.negative", ["candidate_b.neutral"], 0.1)
+
+# auxiliary transitions that polarise attitude of the population
+model.set_transition_fast("candidate_a.neutral", "candidate_a.negative", ["candidate_b.positive"], 0.1)
+model.set_transition_fast("candidate_b.neutral", "candidate_b.positive", ["candidate_a.negative"], 1)
+model.set_transition_fast("candidate_b.neutral", "candidate_b.negative", ["candidate_a.positive"], 0.1)
+model.set_transition_fast("candidate_a.neutral", "candidate_a.positive", ["candidate_b.negative"], 0.1)
+
 
 # initialise experiment
 experiment = MultiSpreading(model, network)
 
 # initialise starting parameters of propagation in network
-experiment.set_initial_states({"marriage": (15, 0), "business": (11, 0)})
-set_node_state(experiment, "marriage", "Ginori", "I")
-set_node_state(experiment, "marriage", "Ridolfi", "I")
-set_node_state(experiment, "marriage", "Medici", "I")
-set_node_state(experiment, "business", "Lamberteschi", "V")
+experiment.set_initial_states({"candidate_a": (2, 146, 2), "candidate_b": (2, 146, 2)})
 
 # perform propagation experiment
 logs = experiment.perform_propagation(n_epochs=200)
